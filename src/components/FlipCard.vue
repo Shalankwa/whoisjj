c1fd3800265353112e2d65c7b79c117f886d4b9c
shalankwa <jonathangoodman.j@gmail.com>
2025-07-20 19:58:06 +0900

    add flip card

---
+++ src/components/FlipCard.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  flipped: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+})
+
+var flipped = ref(props.flipped)
+
+</script>
+
+<template>
+    <div class="flip-card" :class="{ flipped: flipped }" @click="flipped = !flipped">
+        <div class="front">
+            <slot name="front"></slot>
+        </div>
+        <div class="back">
+            <slot name="back"></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.flip-card {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  perspective: 1000px;
+  transition: all 0.5s ease-in-out;
+  transform-style: preserve-3d;
+
+  &.flipped {
+    transform: rotateY(180deg);
+  }
+
+  .front,
+  .back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    backface-visibility: hidden;
+    border-radius: 4px;
+    box-shadow: 0 1px 5px #00000099;
+  }
+
+  .front {
+    background-color: #ac3232;
+    color: #ffffff;
+    transform: rotateY(0deg);
+  }
+
+  .back {
+    background-color: #ffffff;
+    color: #000000;
+    transform: rotateY(180deg);
+  }
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,19 @@
 <script setup>
-import Card from '../components/Card3D.vue'
+import FlipCard from '@/components/FlipCard.vue'
 </script>
 
 <template>
-  <Card>
-
-  </Card>
+  <FlipCard>
+    <template #front>
+      <h1>About</h1>
+      <p>I'm a Software Engineer living in Tokyo, Japan.</p>
+      <p>Take a look around and see what I've been up to!</p>
+    </template>
+    <template #back>
+      <h1>Stuff back</h1>
+      <p>THis is the back side</p>
+    </template>
+  </FlipCard>
 </template>
 
 <style></style>
