029145029b8244c91f13f12f32dcefda3ff2a577
shalankwa <jonathangoodman.j@gmail.com>
2025-07-23 20:35:55 +0900

    Project view WIP

---
+++ src/components/Seal.vue
@@ -0,0 +1,52 @@
+<script setup>
+</script>
+
+<template>
+  <div class="seal">
+    <p>Authentic</p>
+    <div class="aino">NOAINOAI</div>
+    <p>No AI Inside.</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.aino {
+  text-align: center;
+  width: 80%;
+  font-size: 8px;
+  border-radius: 1250px;
+  padding: 2px;
+  border: gold solid 3px;
+}
+.seal {
+  position: relative;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100px;
+  height: 100px;
+  /* background-color: gold; */
+  border: gold solid 10px;
+  border-radius: 50%;
+  font-size: 20px;
+  background-color: black;
+  mask: url(../assets/images/seal_mask.svg) no-repeat center;
+
+  p {
+      padding: 1px 15px;
+      text-align: center;
+      font-size: 6px;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border: rgb(185, 158, 0) solid 2px;
+    border-radius: 50%;
+  }
+}
+</style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,8 +1,83 @@
 <script setup>
+import { onMounted, ref, useTemplateRef } from 'vue';
+import { clamp } from '@/utils/toolbox';
+import Seal from '../components/Seal.vue'
+import Project from '../components/Project.vue'
+
+const itemWidth = 250; 
+const itemWidthpx = itemWidth + 'px';
+
+const projects = ref(["Project1", "Project2", "Project3", "Project4"]);
+const itemRefs = useTemplateRef('items');
+
+var currIndex = 0;
+
+const slider = ref(null);
+const carousel = ref(null);
+
+onMounted(() => {
+  move(currIndex);
+})
+
+function prev() {
+  currIndex = clamp(--currIndex, 0, projects.value.length - 1)
+  move(currIndex);
+}
+
+function next() {
+  currIndex = clamp(++currIndex, 0, projects.value.length - 1)
+  move(currIndex);
+}
+
+function move(index) {
+  slider.value.style.transform = `translate3d(${index * -itemWidth}px, 0, 0)`;
+
+  itemRefs.value.forEach((item, i) => {
+    if(i === index) {
+      item.setRotation("0deg")
+    } else if(i < index) {
+      item.setRotation("40deg")
+    } else {
+      item.setRotation("-40deg")
+    }
+  });
+}
+
 </script>
 
 <template>
-  <h1>Projects</h1>
+  <div class="carousel" ref="carousel">
+    <h1>Projects!</h1>
+    <div class="carousel__slider" ref="slider">
+      <div v-for="project in projects">
+        <Project :name="project" :width="itemWidthpx"  ref="items" /> 
+      </div>
+    </div>
+    <div class="flex justify-center gap-6">
+      <div class="carousel__prev text-4xl cursor-pointer" @click="prev"><</div>
+      <div class="carousel__next text-4xl cursor-pointer" @click="next">></div>
+    </div>
+  </div>
 </template>
 
-<style></style>
+<style scoped lang="scss">
+
+.carousel {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  position: relative;
+
+  &__slider {
+    display: flex;
+    flex-grow: 1;
+    flex-direction: row;
+    width: fit-content;
+    padding: 20px 0;
+    align-items: center;
+    margin-left: calc(50% - (v-bind(itemWidthpx) / 2));
+    transition: all 0.5s ease-in-out;
+  }
+}
+
+</style>
